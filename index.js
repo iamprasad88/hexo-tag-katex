3772059b8c31f19aa3ca68e6b06d5dc497503cd6
prasad <iamprasad88@gmail.com>
2015-11-21 20:31:25 -0800

    Add link to CSS file in posts

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 'use strict';
 
-var katex = require('katex');
+var katex = require('katex'),
+	util = require('hexo-util');
 
 /**
 * {% katex [displayMode=true] %}
 */
 
 hexo.extend.tag.register('katex', function(args, content){
-
 	var displayMode = (args[0] && args[0]!== "true") ? false : true;
 
 	return katex.renderToString(content, {
 		displayMode: displayMode
 	});
-
 }, {ends: true});
+
+/*
+* prepend link to Khan Academy CSS
+*/
+hexo.extend.filter.register('after_post_render', function(data){
+	data.content = util.htmlTag('link',{
+		rel: 'stylesheet',
+		type: 'text/css',
+		href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'
+	}) + data.content;
+
+	return data;
+});
